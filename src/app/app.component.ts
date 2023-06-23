5655d2c23e8596828ac33dd606915368e21af5f5
Zdravko Bozhkov <zdravko6t5@gmail.com>
2023-06-23 18:31:55 -0400

    Added new blog post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CloudinaryImage } from '@cloudinary/url-gen';
+import { fill } from '@cloudinary/url-gen/actions/resize';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-blog';
+  myImage = new CloudinaryImage('sample', { cloudName: 'dppg3f01m' }).resize(
+    fill().width(60).height(60)
+  );
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BioComponent } from './bio/bio.component';
 import { HeadingComponent } from './heading/heading.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CloudinaryModule } from '@cloudinary/ng';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from './loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -15,14 +19,23 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     BioComponent,
     HeadingComponent,
     FooterComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ScullyLibModule
+    ScullyLibModule,
+    CloudinaryModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
